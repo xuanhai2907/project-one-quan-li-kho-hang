4f17d5ea96c9f24f6433cad895966391c0deccba
DemoGit <haipxph26772@fpt.edu.vn>
2022-10-24 13:57:01 +0700

    create project quan li kho hang

---
+++ project-one/src/main/java/com/qlkhohang/QlKhoHangApplication.java
@@ -0,0 +1,11 @@
+package com.qlkhohang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlKhoHangApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QlKhoHangApplication.class, args);
+    }
+}
---
+++ project-one/src/test/java/com/qlkhohang/QlKhoHangApplicationTests.java
@@ -0,0 +1,12 @@
+package com.qlkhohang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class QlKhoHangApplicationTests {
+    @Test
+    void contextLoads(){
+
+    }
+}
